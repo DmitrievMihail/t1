ca1eaff554335d156a0767ec220ea07df1452fa8
Mihail Dmitriev <mmxdmv@mail.ru>
2023-06-17 00:08:15 +0400

    :Добавлены опциональные children и разворот

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React, { FC, useEffect, useState } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
-import Table, {THeadTotal} from './components/Table';
+import Table, {THeadTotal, TBody} from './components/Table';
 import headTotal from './mocks/head1.json';
+
+const tbody = {
+  1: {'a': 5, 'b': 10},
+  5: {'a': 3},
+};
+// body={tbody as TBody}
 
 const App: FC = () => {
   return (
     <div className="App">
-      <Table head={headTotal as THeadTotal}>
+      <Table head={headTotal as THeadTotal} rotate>
         <tr>
-          <td>Ячейка</td>
+          <td>1</td>
+          <td>2</td>
         </tr>
       </Table>
     </div>
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './Table.css';
+import parse from 'html-react-parser';
 
 /* eslint-disable */
 type THeadData = Array<string>;
 type THeadData2 = Array<THeadData>;
+export type TBody = Array<Array<string|number|null>>
 
 export type THeadTotal = [
 	object,
@@ -11,77 +13,60 @@ export type THeadTotal = [
 	Array <Array<Array<number>>>,
 	Array <Array<number>>,
 ];
+
+type TTableHead = Array<React.ReactElement> | null;
 /* eslint-enable */
 
-const Table = ({children, head} : { children: React.ReactElement, head: THeadTotal }) => {
+const Table = ({children, head, rotate} : { children?: React.ReactElement, head: THeadTotal, rotate?: boolean }) => {
+
+	console.log(rotate);
 
 	// eslint-disable-next-line
 	const [fields, headData, headMerge, headDelete] = head;
 
+	let tableHead: TTableHead;
+
 	// console.log(headData);
-	const p: Array<React.ReactElement> = [];
-	for (const headLine in headData) {
-		for (const headCell in headData[headLine]) {
-			if (!((headLine in headDelete) && (headCell in headDelete[headLine]) && headDelete[headLine][headCell])) {
-				const colSpan = (headLine in headMerge) && (headCell in headMerge[headLine]) && (0 in headMerge[headLine][headCell]) ? headMerge[headLine][headCell][0] : 1;
-				const rowSpan = (headLine in headMerge) && (headCell in headMerge[headLine]) && (1 in headMerge[headLine][headCell]) ? headMerge[headLine][headCell][1] : 1;
-				p[headCell] = <td colSpan={colSpan} rowSpan={rowSpan}>{headData[headLine][headCell]}</td>;
+	if (rotate) {
+		const p: Array<Array<React.ReactElement>> = [];
+		for (const headLine in headData) {
+			// console.log(headData[headLine]);
+			for (const headCell in headData[headLine]) {
+				if (!((headLine in headDelete) && (headCell in headDelete[headLine]) && headDelete[headLine][headCell])) {
+					const colSpan = (headLine in headMerge) && (headCell in headMerge[headLine]) && (0 in headMerge[headLine][headCell]) ? headMerge[headLine][headCell][0] : 1;
+					const rowSpan = (headLine in headMerge) && (headCell in headMerge[headLine]) && (1 in headMerge[headLine][headCell]) ? headMerge[headLine][headCell][1] : 1;
+					if (!p[headCell]) {
+						p[headCell] = [];
+					}
+					const id = `head${headLine}-${headCell}`;
+					p[headCell].push(<td
+						key={id}
+						id={id}
+						colSpan={colSpan}
+						rowSpan={rowSpan}
+					>{parse(headData[headLine][headCell])}</td>);
+				}
 			}
 		}
+		tableHead = p.map((x, i) =>
+			<tr className='head' key={`head${i}`} id={`head${i}`}>
+				{x.map((y) => y)}
+			</tr>);
+	} else {
+		tableHead = null;
+
 	}
 
 	return (
       <table className="MainTable">
 		<thead>
-			{p.map((x) => <tr className='head'>{x}</tr>)}
+			{tableHead}
 		</thead>
-        {children}
+		<tbody>
+			{children}
+		</tbody>
       </table>
     );
 };
 
 export default Table;
-
-/*
-
-list($fields,$head_data,$head_merge,$head_delete)=$data[23];
-
-$p=array();
-foreach($head_data AS $hk=>$hl){ // Перевёрнутый заголовок по таблице распихиваем
-	foreach($hl AS $k=>$v){
-		if(!isset($head_delete[$hk][$k])){
-			@$p[$k].="<td class='head' ".(isset($head_merge[$hk][$k])?' rowspan='.$head_merge[$hk][$k][1].' colspan='.$head_merge[$hk][$k][0].' ':'').">".$v."</td>";
-		}
-	}
-}
-
-$line=0;
-foreach($fields AS $k=>$v){
-	$t.="<tr>".$p[$line];
-	$val='';
-	if($v[0]=='+'){
-		foreach(explode(',',substr($v,1,100)) as $vv){
-			@$val+=@$x[$vv];
-		}
-		$t.="<td id='".$k."' class='auto'>".$val."</td>";
-	}elseif($v[0]=='~'){
-		$val=@$x[substr($v,1,100)];
-		$t.="<td id='".$k."' class='auto'>".$val."</td>";
-	}else{
-		if(isset($x[$k])){
-			$val=@$x[$k];
-			if($v==1 and strlen($val)){
-				$val=$val==1?'Да':'Нет';
-			}
-		}
-		if($flag){
-			$t.="<th id='".$k."' class='".$v."'>".$val."</th>";
-		}else{
-			$t.="<td id='".$k."' class='".$v."' style='background:#EEE;text-align:center;' onclick='alert(\"Заблокировано\")'>".$val."</td>";
-		}
-	}
-	$t.="</tr>";
-	$line++;
-}
-
-*/
